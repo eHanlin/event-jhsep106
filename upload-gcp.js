e0ae8b665e9b93b5c07f055f37e416777347cbe4
AsoTiesto <cute17852337569@gmail.com>
2018-06-11 18:59:22 +0800

    完成 upload gcp 及 make public 功能（測試機）

--- upload-gcp.js
+++ upload-gcp.js
@@ -1,10 +1,10 @@
 const Storage = require('@google-cloud/storage')
 const fs = require('fs')
 const path = require('path')
-const gcpKeyFile = `./service-account-file.json`
-const bucketName = `ehanlin-event-test`
-const projectId = `187045169515`
+const bucketName = `tutor-events`
+const projectId = `tutor-204108`
 const projectName = `jhsep106`
+const gcpKeyFile = `./tutor-204108.json`
 
 const storage = new Storage({
   projectId: projectId,
