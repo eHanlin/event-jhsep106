c19f961d94f7a403fc3b89f07038ccee429ce339
AsoTiesto <cute17852337569@gmail.com>
2019-10-14 12:23:27 +0800

    調整功能：調整 gulpfile upload 方式

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,51 @@ const util = require("gulp-template-util");
 const gcPub = require("gulp-gcloud-publish");
 const pngquant = require("imagemin-pngquant");
 
-let bucketNameForTest = "tutor-events-test";
-let bucketNameForProd = "tutor-events";
-let projectId = "tutor-204108";
-let keyFilename = "tutor.json";
-let projectName = "event/jhsep106/";
+const bucketNameForTest = "tutor-test-events";
+const bucketNameForProd = "tutor-events";
+const projectId = "tutor-204108";
+const projectIdTest = "tutor-test-238709";
+const keyFileName = "tutor.json";
+const keyFileNameTest = "tutor-test.json";
+const projectName = "event/jhsep106/";
+
+let uploadGCSProd = bucketName => {
+    return gulp
+        .src(["dist/*.html", "dist/img/**", "dist/css/**"], {
+            base: `${__dirname}/dist/`
+        })
+        .pipe(
+            gcPub({
+                bucket: bucketName,
+                keyFilename: keyFileName,
+                base: projectName,
+                projectId: projectId,
+                public: true,
+                metadata: {
+                    cacheControl: "no-store, no-transform"
+                }
+            })
+        );
+};
+
+let uploadGCSTest = bucketName => {
+    return gulp
+        .src(["dist/*.html", "dist/img/**", "dist/css/**"], {
+            base: `${__dirname}/dist/`
+        })
+        .pipe(
+            gcPub({
+                bucket: bucketName,
+                keyFilename: keyFileNameTest,
+                base: projectName,
+                projectId: projectIdTest,
+                public: true,
+                metadata: {
+                    cacheControl: "no-store, no-transform"
+                }
+            })
+        );
+};
 
 let copyStaticTask = dest => {
     return () => {
@@ -46,40 +86,7 @@ let minifyImage = sourceImage => {
         .pipe(gulp.dest("./dist"));
 };
 
-let uploadGCS = bucketName => {
-    return gulp
-        .src(
-            [
-                "./dist/*.html",
-                "./dist/css/**/*.css",
-                // './dist/js/**/*.js',
-                // './dist/lib/**/*.@(js|json)',
-                "./dist/img/**/*.@(png|jpg|svg|gif)"
-            ],
-            {
-                base: `${__dirname}/dist/`
-            }
-        )
-        .pipe(
-            gcPub({
-                bucket: bucketName,
-                keyFilename: keyFilename,
-                base: projectName,
-                projectId: projectId,
-                public: true,
-                metadata: {
-                    cacheControl: "no-store"
-                }
-            })
-        );
-};
-
-/* upload to gcp test */
-gulp.task("uploadGcpTest", uploadGCS.bind(uploadGCS, bucketNameForTest));
-
-/* upload to gcp test */
-gulp.task("uploadGcpProd", uploadGCS.bind(uploadGCS, bucketNameForProd));
-
+gulp.task("build", ["style", "lib"]);
 gulp.task("package", () => {
     let deferred = Q.defer();
     Q.fcall(() => {
@@ -89,3 +96,6 @@ gulp.task("package", () => {
     });
     return deferred.promise;
 });
+
+gulp.task("uploadGcsTest", uploadGCSTest.bind(uploadGCSTest, bucketNameForTest));
+gulp.task("uploadGcsProd", uploadGCSProd.bind(uploadGCSProd, bucketNameForProd));
